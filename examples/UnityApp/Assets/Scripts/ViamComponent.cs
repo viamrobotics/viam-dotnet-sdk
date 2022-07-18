4d4e0fb27b99e4c8128e4e4cac69d0bbc7078fac
Naveed Jooma <naveed@viam.com>
2022-07-18 14:55:45 -0400

    Two arms, faster updates

--- examples/UnityApp/Assets/Scripts/ViamComponent.cs
+++ examples/UnityApp/Assets/Scripts/ViamComponent.cs
@@ -3,34 +3,34 @@ using Grpc.Net.Client;
 using Grpc.Net.Client.Web;
 using System.Net.Http;
 using Proto.Api.Robot.V1;
+using Proto.Api.Service.Motion.V1;
 using Viam.Net.Sdk.Core;
 using Proto.Rpc.V1;
 using Grpc.Core;
+using Google.Protobuf.WellKnownTypes;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System;
 using System.IO;
 using System.Text;
+using System.Linq;
 
 public class ViamComponent : MonoBehaviour
 {
-    List<GameObject> joints = new List<GameObject>();
-    List<double> jointVals = new List<double>();
+    List<GameObject> leftJoints = new List<GameObject>();
+    List<double> leftJointVals = new List<double>();
+    List<GameObject> rightJoints = new List<GameObject>();
+    List<double> rightJointVals = new List<double>();
+
+    bool ready = false;
+    bool initialSet = false;
+
+
+    private readonly Proto.Api.Common.V1.ResourceName leftArmResourceName = new Proto.Api.Common.V1.ResourceName() { Namespace = "rdk", Type = "component", Subtype = "arm", Name = "left_arm" };
+    private readonly Proto.Api.Common.V1.ResourceName rightArmResourceName = new Proto.Api.Common.V1.ResourceName() { Namespace = "rdk", Type = "component", Subtype = "arm", Name = "right_arm" };
 
     async void Start()
     {
-        GameObject last = null;
-        for (int i = 0; i < 6; i++)
-        {
-            GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-            cylinder.transform.parent = last?.transform;
-            cylinder.transform.localPosition = new Vector3(0, 2, 0);
-
-            joints.Add(cylinder);
-            jointVals.Add(0);
-            last = cylinder;
-        }
-
         UnitySystemConsoleRedirector.Redirect();
         var logger = NLog.LogManager.GetCurrentClassLogger();
 
@@ -57,10 +57,27 @@ public class ViamComponent : MonoBehaviour
 
                     Debug.Log(await robotClient.ResourceNamesAsync(new ResourceNamesRequest()));
 
+
+                    var initialStatus = await robotClient.GetStatusAsync(new GetStatusRequest
+                    {
+                        ResourceNames = {
+                            leftArmResourceName,
+                            rightArmResourceName
+                        }
+                    });
+
+                    leftJointVals = initialStatus.Status[0].Status_.Fields["joint_positions"].StructValue.Fields["values"].ListValue.Values.Clone().Select(obj => obj.NumberValue).ToList();
+                    rightJointVals = initialStatus.Status[1].Status_.Fields["joint_positions"].StructValue.Fields["values"].ListValue.Values.Clone().Select(obj => obj.NumberValue).ToList();
+
+                    this.ready = true;
+
                     var statusRespStream = robotClient.StreamStatus(new StreamStatusRequest
                     {
                         ResourceNames = {
-                        new Proto.Api.Common.V1.ResourceName() { Namespace = "rdk", Type = "component", Subtype = "arm", Name = "arm1" } }
+                            leftArmResourceName,
+                            rightArmResourceName
+                        },
+                        Every = Duration.FromTimeSpan(TimeSpan.FromSeconds(1.0 / 60))
                     });
 
                     while (true)
@@ -70,11 +87,18 @@ public class ViamComponent : MonoBehaviour
                             break;
                         }
                         var cur = statusRespStream.ResponseStream.Current;
-                        var jointVals = cur.Status[0].Status_.Fields["joint_positions"].StructValue.Fields["values"].ListValue.Values.Clone();
-                        for (int i = 0; i < joints.Count; i++)
+
+                        for (int i = 0; i < cur.Status.Count; i++)
                         {
-                            // TODO(erd): make thread safe
-                            this.jointVals[i] = jointVals[i].NumberValue;
+                            var status = cur.Status[i];
+                            if (status.Name.Equals(leftArmResourceName))
+                            {
+                                leftJointVals = status.Status_.Fields["joint_positions"].StructValue.Fields["values"].ListValue.Values.Clone().Select(obj => obj.NumberValue).ToList();
+                            }
+                            else
+                            {
+                                rightJointVals = status.Status_.Fields["joint_positions"].StructValue.Fields["values"].ListValue.Values.Clone().Select(obj => obj.NumberValue).ToList();
+                            }
                         }
                     }
                 }
@@ -82,12 +106,52 @@ public class ViamComponent : MonoBehaviour
         });
     }
 
+    void setArmStartingPositions(List<GameObject> joints, int len, bool isLeft)
+    {
+
+        GameObject last = null;
+        for (int i = 0; i < len; i++)
+        {
+            GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            cylinder.transform.parent = last?.transform;
+            cylinder.transform.localPosition = new Vector3(0, 2, 0);
+            if (i == 0)
+            {
+                cylinder.transform.localPosition = new Vector3(isLeft ? 10 : 0, 2, 0);
+            }
+
+            joints.Add(cylinder);
+            last = cylinder;
+        }
+    }
+
     void Update()
     {
-        for (int i = 0; i < joints.Count; i++)
+
+        if (!ready)
+        {
+            return;
+        }
+
+        if (!initialSet)
+        {
+            setArmStartingPositions(leftJoints, leftJointVals.Count, true);
+            setArmStartingPositions(rightJoints, rightJointVals.Count, false);
+
+
+            initialSet = true;
+        }
+
+        for (int i = 0; i < leftJoints.Count; i++)
         {
             // TODO(erd): make thread safe
-            joints[i].transform.rotation = Quaternion.Euler((float)jointVals[i], 0, 0);
+            leftJoints[i].transform.rotation = Quaternion.Euler((float)leftJointVals[i], 0, 0);
+        }
+
+        for (int i = 0; i < rightJoints.Count; i++)
+        {
+            // TODO(erd): make thread safe
+            rightJoints[i].transform.rotation = Quaternion.Euler((float)rightJointVals[i], 0, 0);
         }
     }
 }
@@ -147,4 +211,3 @@ public static class UnitySystemConsoleRedirector
         Console.SetOut(new UnityTextWriter());
     }
 }
-
